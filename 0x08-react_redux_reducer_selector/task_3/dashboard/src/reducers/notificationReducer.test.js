a9df57e0e172b15214f1c5f021c3c734eb2738ab
Chy-baby <chideraikpeama@gmail.com>
2024-06-08 03:50:26 +0000

    my project

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.test.js
@@ -0,0 +1,23 @@
+{
+  filter: "DEFAULT",
+  notifications: [
+    {
+      id: 1,
+      isRead: false,
+      type: "default",
+      value: "New course available"
+    },
+    {
+      id: 2,
+      isRead: false,
+      type: "urgent",
+      value: "New resume available"
+    },
+    {
+      id: 3,
+      isRead: false,
+      type: "urgent",
+      value: "New data available"
+    }
+  ]
+}
